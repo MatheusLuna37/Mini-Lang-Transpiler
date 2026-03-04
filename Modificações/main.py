057faff89fdb9d7c8dce7183905850a91c1e143a
wal <wal@WEED>
2026-03-03 21:50:25 -0300

    main para testar o scanner

---
+++ Modificações/main.py
@@ -0,0 +1,9 @@
+with open('programa_teste.ml', 'r') as f:
+    conteudo = f.read()
+
+lista_tokens = scanner(conteudo)
+
+print(f"{'LINHA':<7} | {'TIPO':<10} | {'VALOR'}")
+print("-" * 30)
+for t in lista_tokens:
+    print(f"{t.line:<7} | {t.type:<10} | {t.value}")
