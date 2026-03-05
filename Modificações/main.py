2d3c5f55f46f2faf0450fa7f9adbf8e041adee49
wal <wal@WEED>
2026-03-04 21:01:59 -0300

    main para testar o parser

--- Modificações/main.py
+++ Modificações/main.py
@@ -7,3 +7,7 @@ print(f"{'LINHA':<7} | {'TIPO':<10} | {'VALOR'}")
 print("-" * 30)
 for t in lista_tokens:
     print(f"{t.line:<7} | {t.type:<10} | {t.value}")
+
+parser = Parser(lista_tokens)
+ast = parser.parse_program()
+print("Sucesso! AST gerada.")
